d2753f3545a27619755ce2cdc2ee73b09490816b
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-11-12 03:51:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,30 @@ const port = 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'files')));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'files')));
 
 app.get('/', async (req, res) => {
     res.render('home', {});
 });
 
-app.get('/purchase', async (re, res) => {
-    res.render('purchase', {});
-});
-app.get('/trade', async (re, res) => {
-    res.render('trade', {});
-});
-app.get('/wallet', async (re, res) => {
-    res.render('wallet', {});
-});
-app.get('/market', async (re, res) => {
-    res.render('market', {});
-});
-app.get('/learn', async (re, res) => {
-    res.render('learn', {});
-});
-app.get('/dashboard', async (re, res) => {
-    res.render('dashboard', {});
-});
+// app.get('/purchase', async (re, res) => {
+//     res.render('purchase', {});
+// });
+// app.get('/trade', async (re, res) => {
+//     res.render('trade', {});
+// });
+// app.get('/wallet', async (re, res) => {
+//     res.render('wallet', {});
+// });
+// app.get('/market', async (re, res) => {
+//     res.render('market', {});
+// });
+// app.get('/learn', async (re, res) => {
+//     res.render('learn', {});
+// });
+// app.get('/dashboard', async (re, res) => {
+//     res.render('dashboard', {});
+// });
 
 app.listen(port, () => {
     console.log(`Server started on port`);
